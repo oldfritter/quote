1a3c1eb8ada7892d0ff989c7f532b1fb73d88ff8
Leon Oldfritter <leon.zcf@gmail.com>
2020-06-03 16:59:09 +0800

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 nohup ./cmd/api >> logs/api.log &
+nohup ./cmd/ws >> logs/ws.log &
 nohup ./cmd/schedule >> logs/schedule.log &
 nohup ./cmd/workers >> logs/workers.log &
 nohup ./cmd/getData >> logs/getData.log &
