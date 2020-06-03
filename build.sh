efaa867469573934104565f65508211d79a7260d
Leon Oldfritter <leon.zcf@gmail.com>
2020-06-03 16:37:55 +0800

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 go build -o ./cmd/getData sourceData/getData.go
 go build -o ./cmd/api api/api.go
+go build -o ./cmd/worker workers/worker.go
+go build -o ./cmd/schedule schedules/schedule.go
--- stop.sh
+++ stop.sh
@@ -1,2 +1,4 @@
 cat pids/api.pid  | xargs kill -INT
 cat pids/getData.pid  | xargs kill -INT
+cat pids/schedule.pid  | xargs kill -INT
+cat pids/worker.pid  | xargs kill -INT
