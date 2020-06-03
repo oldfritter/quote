d9779e5f30afccbe87c016c687bcccf8dfe6c149
Leon Oldfritter <leon.zcf@gmail.com>
2020-06-03 16:44:03 +0800

    Fix

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-nohup ./cmd/api &
-nohup ./schedules/schedule &
-nohup ./workers/worker &
-nohup ./cmd/getData &
+nohup ./cmd/api >> logs/api.log &
+nohup ./schedules/schedule >> logs/schedule.log &
+nohup ./workers/worker >> logs/worker.log &
+nohup ./cmd/getData >> logs/getData.log &
