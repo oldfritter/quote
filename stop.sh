0a5603a988c7c255ec92569f230a14b1c5db83a8
Leon Oldfritter <leon.zcf@gmail.com>
2020-06-04 14:46:44 +0800

    update

--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 cat pids/api.pid  | xargs kill -INT
-cat pids/ws.pid  | xargs kill -INT
+cat pids/ws.pid  | xargs kill -9
 cat pids/getData.pid  | xargs kill -INT
 cat pids/schedule.pid  | xargs kill -INT
 cat pids/workers.pid  | xargs kill -INT
