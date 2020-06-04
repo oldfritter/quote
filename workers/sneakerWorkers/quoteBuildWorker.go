3fe032c13464072edafc7e76265cfd416c2ff7dc
Leon Oldfritter <leon.zcf@gmail.com>
2020-06-04 15:14:48 +0800

    update

--- workers/sneakerWorkers/quoteBuildWorker.go
+++ workers/sneakerWorkers/quoteBuildWorker.go
@@ -40,11 +40,13 @@ func (worker Worker) SubQuoteBuildWorker(payloadJson *[]byte) (err error) {
 		}
 		subQuotes = append(subQuotes, sub)
 	}
-	// if payload.Level == 0 {
-	//   for _, q := range subQuotes {
-	//     createSubQuote(&q, payload.Level+1)
-	//   }
-	// }
+	if payload.Level == 0 {
+		for _, q := range subQuotes {
+			if subQuotes.Quote == "usd" {
+				createSubQuote(&q, payload.Level+1)
+			}
+		}
+	}
 	return
 }
 
