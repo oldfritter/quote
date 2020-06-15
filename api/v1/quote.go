adcf9597aa2577e1b38a5360c865b63a28bce07c
Leon Oldfritter <leon.zcf@gmail.com>
2020-06-15 16:47:12 +0800

    fix

--- api/v1/quote.go
+++ api/v1/quote.go
@@ -16,7 +16,7 @@ type Result struct {
 func GetApiQuotes(context echo.Context) error {
 	db := utils.DbBegin()
 	defer db.DbRollback()
-	var coins, result []Currency
+	var coins []Currency
 	var symbols, currencies, sources []string
 	if context.QueryParam("symbols") != "" {
 		symbols = strings.Split(context.QueryParam("symbols"), ",")
@@ -24,19 +24,11 @@ func GetApiQuotes(context echo.Context) error {
 	if context.QueryParam("currencies") != "" {
 		currencies = strings.Split(context.QueryParam("currencies"), ",")
 	}
-	if context.QueryParam("currency") != "" {
-		currencies = strings.Split(context.QueryParam("currency"), ",")
-	}
 	for _, source := range strings.Split(context.QueryParam("sources"), ",") {
 		if source != "" {
 			sources = append(sources, "Quotes::"+strings.Title(source))
 		}
 	}
-	// for _, source := range strings.Split(context.QueryParam("source"), ",") {
-	//   if source != "" {
-	//     sources = append(sources, "Quotes::"+strings.Title(source))
-	//   }
-	// }
 	if db.Where("symbol in (?)", symbols).Find(&coins).RecordNotFound() {
 		return utils.BuildError("1020")
 	}
@@ -49,11 +41,8 @@ func GetApiQuotes(context echo.Context) error {
 			conditions = conditions.Joins("INNER JOIN (currencies) ON (currencies.id = quotes.quote_id)").Where("currencies.symbol in (?)", currencies)
 		}
 		conditions.Find(&coins[i].Quotes)
-		if len(coins[i].Quotes) > 0 {
-			result = append(result, coins[i])
-		}
 	}
 	response := utils.ArrayResponse
-	response.Body = &Result{Data: result}
+	response.Body = &Result{Data: coins}
 	return context.JSON(http.StatusOK, response)
 }
